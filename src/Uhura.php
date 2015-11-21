be4056134ea9371218a35eef264b96d6ddce4598
Colin DeCarlo <colin@thedecarlos.ca>
2015-11-21 12:38:13 -0500

    Add simple CRUD to Uhura

--- src/Uhura.php
+++ src/Uhura.php
@@ -2,14 +2,56 @@
 
 namespace Uhura;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Handler\MockHandler;
+
 class Uhura
 {
     private $api = '';
     private $resource = '';
+    private $http;
 
     public function __construct($api)
     {
         $this->api = $api;
+        $this->http = new Client(['base_uri' => $this->api]);
+    }
+
+    public static function test($api)
+    {
+        $uhura = new static($api);
+
+        $uhura->http = new Client([
+            'base_uri' => $api,
+            'handler' => new MockHandler
+        ]);
+
+        return $uhura;
+    }
+
+    public function getHttp()
+    {
+        return $this->http;
+    }
+
+    public function get()
+    {
+        return $this->http->get($this->resource);
+    }
+
+    public function create($payload)
+    {
+        return $this->http->post($this->resource, ['form_params' => $payload]);
+    }
+
+    public function update($payload)
+    {
+        return $this->http->put($this->resource, ['form_params' => $payload]);
+    }
+
+    public function delete()
+    {
+        return $this->http->delete($this->resource);
     }
 
     public function url()
--- tests/UhuraTest.php
+++ tests/UhuraTest.php
@@ -2,13 +2,16 @@
 
 use Uhura\Uhura;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7\Response;
+
 class UhuraTest extends PHPUnit_Framework_TestCase
 {
     public $uhura;
 
     public function setUp()
     {
-        $this->uhura = new Uhura('http://example.com');
+        $this->uhura = Uhura::test('http://example.com');
     }
 
     public function test_that_uhura_can_return_the_url_of_a_resource()
@@ -33,5 +36,69 @@ class UhuraTest extends PHPUnit_Framework_TestCase
             'http://example.com/users',
             $this->uhura->users->url()
         );
+    }
+
+    public function test_that_uhura_sends_a_get_request_for_the_correct_resource()
+    {
+        $handler = $this->uhura->getHttp()->getConfig('handler');
+        $handler->append(new Response);
+
+        $this->uhura->users(1)->get();
+
+        $this->assertEquals(0, $handler->count());
+        $this->assertEquals('GET', $handler->getLastRequest()->getMethod());
+        $this->assertEquals('http://example.com/users/1', $handler->getLastRequest()->getUri());
+    }
+
+    public function test_that_uhura_sends_a_post_reequest_with_the_correct_parameters_when_creating_resources()
+    {
+        $handler = $this->uhura->getHttp()->getConfig('handler');
+        $handler->append(new Response);
+
+        $user = [
+            'email' => 'test@example.com',
+        ];
+
+        $this->uhura->users->create($user);
+
+        $this->assertEquals(0, $handler->count());
+        $this->assertEquals('POST', $handler->getLastRequest()->getMethod());
+        $this->assertEquals('http://example.com/users', $handler->getLastRequest()->getUri());
+
+        $params = [];
+        parse_str($handler->getLastRequest()->getBody()->getContents(), $params);
+        $this->assertEquals($user, $params);
+    }
+
+    public function test_that_uhura_sends_a_put_request_with_the_correct_parameters_when_updating_a_resource()
+    {
+        $handler = $this->uhura->getHttp()->getConfig('handler');
+        $handler->append(new Response);
+
+        $user = [
+            'email' => 'test@example.com',
+        ];
+
+        $this->uhura->users(1)->update($user);
+
+        $this->assertEquals(0, $handler->count());
+        $this->assertEquals('PUT', $handler->getLastRequest()->getMethod());
+        $this->assertEquals('http://example.com/users/1', $handler->getLastRequest()->getUri());
+
+        $params = [];
+        parse_str($handler->getLastRequest()->getBody()->getContents(), $params);
+        $this->assertEquals($user, $params);
+    }
+
+    public function test_that_uhura_sends_a_delete_request_to_the_correct_url_when_deleting_a_resource()
+    {
+        $handler = $this->uhura->getHttp()->getConfig('handler');
+        $handler->append(new Response);
+
+        $this->uhura->users(1)->delete();
+
+        $this->assertEquals(0, $handler->count());
+        $this->assertEquals('DELETE', $handler->getLastRequest()->getMethod());
+        $this->assertEquals('http://example.com/users/1', $handler->getLastRequest()->getUri());
     }
 }
